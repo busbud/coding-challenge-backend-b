e45f156b6e079d91b74828c65ea4f4a9ffe22b41
Curtis McEnroe <programble@gmail.com>
2013-11-22 13:44:57 -0500

    Add content type and validate request parameters

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ require 'json'
 class App < Sinatra::Base
   # Endpoints
   get '/suggestions' do
+    content_type :json
+
+    query, lat, long = params[:q], params[:latitude], params[:longitude]
+    halt 400, {:error => 'No query'}.to_json unless query
+    halt 400, {:error => 'Missing coordinate'}.to_json if lat.nil? ^ long.nil?
+    begin
+      lat = Float(lat) if lat
+      long = Float(long) if long
+    rescue ArgumentError
+      halt 400, {:error => 'Invalid coordinates'}.to_json
+    end
+
     status 404
     {:suggestions => []}.to_json
   end
