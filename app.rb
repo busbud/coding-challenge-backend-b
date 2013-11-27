b2dac6dcffe4f98649e89fc148b8c4cc0a63a42c
Simon Law <sfllaw@sfllaw.ca>
2013-11-27 14:20:38 -0500

    Initial tests pass.
    We prepare by creating a stubbed Cities class that will contain all city
    information. We then hook up the /suggestions endpoint to this class.

--- app.rb
+++ app.rb
@@ -1,11 +1,46 @@
 require 'sinatra/base'
 require 'json'
 
+
+# Stubbed Cities data structure to make tests pass
+class Cities
+  def initialize()
+    @data = {
+      "montreal" => {
+        :name => "Montreal, QC, Canada",
+        :latitude => "45.5000",
+        :longitude => "-73.5667",
+      }
+    }
+  end
+
+  def find(prefix)
+    return [] if not prefix or prefix == ""
+    prefix = prefix.downcase
+
+    @data.select{|k, v| k.start_with?(prefix)}
+  end
+
+  def suggest(prefix)
+    suggestions = find(prefix).values.map do |e|
+      score = 1.0
+      e.merge({:score => score})
+    end
+
+    suggestions.sort_by{|e| e[:score]}.reverse!
+  end
+end
+
+
 # http://www.sinatrarb.com/
 class App < Sinatra::Base
   # Endpoints
   get '/suggestions' do
-    status 404
-    {:suggestions => []}.to_json
+    suggestions = @@cities.suggest(params["q"])
+    status 404 if suggestions.empty?
+    {:suggestions => suggestions}.to_json
   end
+
+  # Static database initialized on module load
+  @@cities = Cities.new
 end
