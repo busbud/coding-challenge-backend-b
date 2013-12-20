76d0751fcf9b9ffe1389c160a210d1e5416c737f
Marc-Olivier Alain <marc-olivieralain@MacBook-Pro-de-Marc-Olivier.local>
2013-12-20 11:46:19 -0500

    test initial commit

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'json'
 # http://www.sinatrarb.com/
 class App < Sinatra::Base
   # Endpoints
+  #test initial commit
   get '/suggestions' do
     status 404
     {:suggestions => []}.to_json
