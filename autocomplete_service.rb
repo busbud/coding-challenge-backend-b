27bf84a9c0cf869cd926372daa016299b3dd2e3c
Marc-Olivier Alain <marc-olivieralain@MacBook-Pro-de-Marc-Olivier.local>
2013-12-20 15:35:37 -0500

    some refactoring, add first draft alto(without using lat/long)

--- autocomplete_service.rb
+++ autocomplete_service.rb
@@ -13,12 +13,9 @@ class AutocompleteService
 		if keyword.empty?
 			return []		
 		else
-			test = @repository.getSuggestionsWithParams(keyword)
-			formatedSuggestions = []
-			formatedSuggestions << {
-				:name => test.first[:name]
-			}
-			return formatSuggestions(test)
+			suggestions = @repository.getSuggestionsWithParams(keyword)
+			suggestions.size>0? updatedSuggestions = updateScores(suggestions,keyword) : updatedSuggestions =[];
+			return formatSuggestions(updatedSuggestions)
 		end
 	end
 
@@ -26,40 +23,29 @@ class AutocompleteService
 	def formatSuggestions(cities)
 		formatedSuggestions =[]
 		cities.each do |city|
-			(city[:country]=='CA')? country='CANADA'  : country="USA"
-			country =="CANADA"? state = getProvince(city[:state]) : state = city[:state] 
-			
 			formatedSuggestions << {
-				:name => city[:name]+', '+state+', '+country,
+				:name => city[:name]+', '+city[:state]+', '+city[:country],
 				:latitude => city[:latitude],
 				:longitude => city[:longitude],
 				:score => city[:score],
 			}
 		end
+		return formatedSuggestions.sort_by{ |city| -city[:score].to_f }
+	end
+
+
+
+	def updateScores(cities,keyword)
+
+		largestPopulation = ((cities.sort_by { |city| city[:population].to_i }).last)[:population].to_i
 		
-		return formatedSuggestions
+		cities.each do |city|
+			score = 0.5*(keyword.length.to_f / city[:name].length.to_f) + 0.5*(city[:population].to_f/largestPopulation.to_f)
+			city[:score] = score.round(1)
+		end 
+
+		return cities
 	end
 
-	def getProvince(provinceIndex)
-		province = case provinceIndex.to_i	
-			when 1 then 'AB'
-			when 2 then 'BC'
-			when 3 then 'MB'
-			when 4 then 'NB'
-			when 5 then 'NL'
-			when 6 then 'NS'
-			when 7 then 'NT'
-			when 8 then 'NU'
-			when 9 then 'ON'
-			when 10 then 'PE'
-			when 11 then 'QC'
-			when 12 then 'SK'
-			when 13 then 'YT'
-		end	
-	end
-
-	def updateScores(cities)
-		#TODO add confidence
-	end
 
 end
--- data_parser.rb
+++ data_parser.rb
@@ -8,18 +8,37 @@ class DataParser
 		data = rawData.split("\n")
 
 		data.map do |row|
-		splittedRow=row.split("\t")
-		tableCities << {
-			:id => splittedRow[0], 
-			:name => splittedRow[2],
-			:latitude => splittedRow[4],
-			:longitude => splittedRow[5],
-			:country => splittedRow[8],
-			:state => splittedRow[10],
-			:population => splittedRow[14],
-			:score => 1
-		} 
+			splittedRow=row.split("\t")
+			splittedRow[8]=='CA'? country='CANADA'  : country="USA"
+			country =="CANADA"? state = getProvince(splittedRow[10]) : state = splittedRow[10] 
+			tableCities << {
+				:id => splittedRow[0], 
+				:name => splittedRow[2],
+				:latitude => splittedRow[4],
+				:longitude => splittedRow[5],
+				:country => country,
+				:state => state,
+				:population => splittedRow[14],
+				:score => 1
+			} 
 		end
 		return tableCities
+	end
+
+	def getProvince(provinceIndex)
+		province = case provinceIndex.to_i	
+			when 1 then 'AB'
+			when 2 then 'BC'
+			when 3 then 'MB'
+			when 4 then 'NB'
+			when 5 then 'NL'
+			when 7 then 'NS'
+			when 8 then 'ON'
+			when 9 then 'PE'
+			when 10 then 'QC'
+			when 11 then 'SK'
+			when 12 then 'YT'
+			when 13 then 'NT'
+		end	
 	end
 end
