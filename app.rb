7ba02409019a8feff63df12008be7f63328f58ab
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-02-08 21:37:03 +0100

    Configure redis

--- app.rb
+++ app.rb
@@ -3,21 +3,48 @@ require 'sinatra/base'
 require 'json'
 require 'lib/parse_datas'
 require 'lib/suggestion'
+require 'redis'
 
 file   = File.join(File.dirname(__FILE__), "data", "cities_canada-usa.tsv")
 $cities = ParseDatas.get_datas_from_csv(file)
 
 # http://www.sinatrarb.com/
 class App < Sinatra::Base
+  configure :production do
+    uri    = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
+
+  configure :test do
+    $redis = Redis.new(url: ENV["WERCKER_REDIS_URL"], driver: :hiredis)
+  end
+
+  configure :development do
+    $redis = Redis.new()
+  end
 
   get '/suggestions' do
-    suggestion = Suggestion.new($cities, params)
+    redis_cache_key_for(request.fullpath) do
+      suggestion = Suggestion.new($cities, params)
 
-    halt 404, suggestion.errors if suggestion.errors?
+      halt 404, suggestion.errors if suggestion.errors?
 
-    suggestions = suggestion.results
-    halt 404, {:suggestions => []}.to_json if suggestions.empty?
+      {"suggestions" => suggestion.results}
+    end
+  end
 
-    suggestions = {:suggestions => suggestions}.to_json
+  def redis_cache_key_for(key, &block)
+    results = if $redis.exists(request.fullpath)
+      JSON.parse($redis.smembers(request.fullpath).first)
+    else
+      results = yield
+
+      $redis.sadd(request.fullpath, results.to_json)
+      results
+    end
+
+    halt 404, results.to_json if results["suggestions"].empty?
+
+    results.to_json
   end
 end
