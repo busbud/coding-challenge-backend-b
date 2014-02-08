b17a724a9b4fe745cdfac8ddd33d10db4052c4a0
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-02-08 17:26:19 +0100

    Add ParseDatas Class This class permit to store cities files in an array for future search. All cities where population < ParseDatas::MAX_POPULATIONS are excluded ``` cities = ParseDatas.get_datas_from_csv(file_path) ```

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'sinatra/base'
 require 'json'
+require 'lib/parse_datas'
+
+file   = File.join(File.dirname(__FILE__), "data", "cities_canada-usa.tsv")
+$cities = ParseDatas.get_datas_from_csv(file)
 
 # http://www.sinatrarb.com/
 class App < Sinatra::Base
-  # Endpoints
+  suggestion = Suggestion.new(cities)
+
   get '/suggestions' do
     status 404
     {:suggestions => []}.to_json
